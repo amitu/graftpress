a54f9880722d251d4b7b6023185340052d427011
nilinswap <nilinswap@gmail.com>
2019-07-15 15:05:37 +0530

    forward compiles

--- src/forward.rs
+++ src/forward.rs
@@ -1,8 +1,24 @@
+use crate::routes;
+use realm::utils::{get_slash_complete_path, get, sub_string};
+use serde_json::Value as JsonValue;
+use std::collections::HashMap;
+use url::Url;
+
+
 pub fn magic(req: &realm::Request) -> realm::Result {
-    let path = realm::utils::get_slash_complete_path(req.uri().path());
+    let url = req.uri();
+    let site_url = "127.0.0.1:3000/".to_string();
+    let path = get_slash_complete_path(url.path());
+    let url = Url::parse(&format!("{}{}", &site_url, req.uri()).as_str())?;
+    let mut rest = sub_string(path.as_ref(), path.len(), None);
+    let data_: JsonValue  = serde_json::from_slice(req.body().as_slice())?;
+    let query_: HashMap<_, _> = url.query_pairs().into_owned().collect();
+
     match path.as_ref() {
-        "/" => crate::routes::index::layout(req, 0),
-        url_ => crate::cms::layout(req, url_),
+        "/" => {
+            let i = get("i", &query_, &data_, &mut rest, false)?;
+            routes::index::layout(req, i)
+        },
+        _ => unimplemented!()
     }
 }
-
